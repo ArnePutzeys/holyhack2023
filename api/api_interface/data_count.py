0b92347d6ccf55bf7ff31e3a595df00aef8b7a48
Kevin Oyen <kevin.oyen@student.kuleuven.be>
2023-03-11 14:55:38 +0100

    graph values and subjectivity in return

--- api/api_interface/data_count.py
+++ api/api_interface/data_count.py
@@ -4,30 +4,30 @@
 from data_load import data_load, data_load2, data_load3, data_load4
 from sentiment_dutch import Sentiment_dutch
 from sentiment_analysis2 import Sentiment
-from multiprocessing import Process, Queue
-import multiprocessing
-import numpy as np
 import time
 
 def count(input, file, startdate=None, enddate=None, lines=None):
+    english = False
     #count = {element: [0, 0, Sentiment_dutch()] for element in input}
     if file.endswith('reviews_pakket.json'):
-        count = {element: [0, 0, Sentiment_dutch()] for element in input}
+        count = {element: [0, 0, Sentiment_dutch(), [], []] for element in input}
     else:
-        count = {element: [0, 0, Sentiment()] for element in input}
+        english = True
+        count = {element: [0, 0, Sentiment(), [], []] for element in input}
 
     start_time = time.time()
     start_time_tot = time.time()
+    graph_values = dict()
     if startdate != None and enddate != None:
         if lines == None:
-            sentences, score, iterations = data_load2(file, ['opinion', 'score'], startdate, enddate, lines)
+            sentences, score, iterations, graph_values = data_load2(file, ['opinion', 'score'], startdate, enddate, lines)
         else:
-            sentences, score, iterations = data_load2(file, ['opinion', 'score'], startdate, enddate, 0)
+            sentences, score, iterations, graph_values = data_load2(file, ['opinion', 'score'], startdate, enddate, 0)
     else:
         if lines == None:
-            sentences, score, iterations = data_load4(file, ['opinion', 'score'], None,None, 0, input)
+            sentences, score, iterations, graph_values = data_load4(file, ['opinion', 'score'], None,None, 0, input)
         else:
-            sentences, score, iterations = data_load4(file, ['opinion', 'score'], None,None, lines,input)
+            sentences, score, iterations, graph_values = data_load4(file, ['opinion', 'score'], None,None, lines,input)
     end_time = time.time()
     print("elapsed time: "+str(end_time-start_time))
     start_time = time.time()
@@ -36,6 +36,8 @@ def count(input, file, startdate=None, enddate=None, lines=None):
         for word in input:
             if word in sentences[i].lower():
                 count[word][0] += 1
+                # print(count[word][1])
+                # print(score[i])
                 count[word][1] += int(score[i])
                 if total_limit < 100:
                     count[word][2].compute_sentiment(sentences[i])
@@ -51,34 +53,9 @@ def count(input, file, startdate=None, enddate=None, lines=None):
     totaal = []
     procent = []
     polariteit = []
-    sentiment = []
+    subjectivity = []
     gem_review = []
-    # try: 
-    #     count[element][1] = count[element][1] / count[element][0]
-    #     print(count[element][1])
-    # except ZeroDivisionError:
-    #     count[element][1] = 0 
-    # try:
-    #     totaal = [count[element][0] for element in count]
-    # except ZeroDivisionError:
-    #     totaal.append(0)
-    # try:
-    #     procent = [t / iterations for t in totaal]
-    # except ZeroDivisionError:
-    #     procent.append(0)
-    # try:
-    #     #polariteit = [count[element][2].get_sentiment(t)['average_polarity'] for element, t in zip(count, totaal)]
-    #     polariteit = count[element][2].get_sentiment(t)['average_polarity']
-    # except ZeroDivisionError:
-    #     polariteit.append(0)
-    # try:    
-    #     sentiment = [count[element][2].get_sentiment(t)['overall_sentiment'] for element, t in zip(count, totaal)]
-    # except ZeroDivisionError:
-    #     sentiment.append(0)
-    # try:
-    #     gem_review = [count[element][1] for element in count]
-    # except ZeroDivisionError:
-    #     gem_review.append(0)
+
     for element in count.keys():
         totaal.append(count[element][0])
         try:
@@ -86,9 +63,12 @@ def count(input, file, startdate=None, enddate=None, lines=None):
         except ZeroDivisionError:
             polariteit.append(0)
         try:
-            sentiment.append(count[element][2].get_sentiment(count[element][0])['overall_sentiment'])
+            if english:
+                subjectivity.append(count[element][2].get_sentiment(count[element][0])['average_subjectivity'])
+            else:
+                subjectivity.append(0)
         except ZeroDivisionError:
-            sentiment.append(0)
+            subjectivity.append(0)
         try:
             gem_review.append(count[element][1]/count[element][0])
         except ZeroDivisionError:
@@ -96,21 +76,25 @@ def count(input, file, startdate=None, enddate=None, lines=None):
 
     for i in range(len(count)):
         procent.append(totaal[i]/iterations)
-
-              
+        
+        x_values = []
+        y_values = []
+        for element in graph_values:
+            x_values.append(element)
+            y_values.append(graph_values[element])
 
     # (keywords), hoe_vaak_totaal, hoe_vaak_procent, polariteit, (subjectiviteit), gem_review, tijdsvoorkomen
-    print(keywords,totaal,procent,polariteit,sentiment,gem_review,tijdsvoorkomen)
-    return keywords,totaal,procent,polariteit,sentiment,gem_review,tijdsvoorkomen
+    print(keywords,totaal,procent,polariteit,subjectivity,gem_review,tijdsvoorkomen,x_values,y_values)
+    return keywords,totaal,procent,polariteit,subjectivity,gem_review,tijdsvoorkomen, x_values, y_values
             
 if __name__ == "__main__":
-    #input = ['status','quit']
-    input = ['Vinted-app', 'verzending', 'bezorging', 'post', 'levering', 'opruimen', 'Pakket']
+    input = ['status','quit']
+    #input = ['Vinted-app', 'verzending', 'bezorging', 'post', 'levering', 'opruimen', 'Pakket']
     #input = ['quality']
     for i in range(len(input)):
         input[i] = input[i].lower()
 
     file = '../../data/reviews_pakket.json'
-    #file = 'data/reviews_spotify.csv'
+    #file = '../../data/spotify_data.json'
     count(input, file,None,None,20000)
-    #count(input, file, '2020-11-19', '2020-12-30')
+    #count(input, file, '2020-11-19', '2022-12-30')
--- api/api_interface/data_load.py
+++ api/api_interface/data_load.py
@@ -1,6 +1,7 @@
 import json
 import csv
 import numpy as np
+import datetime
 
 
 def get_date(date):
@@ -63,6 +64,7 @@ def data_load2(path, argument, startdate=None, enddate=None, number_of_reviews=0
         enddate = enddate.split('-')
     output = []
     score = []
+    graph_values = dict()
     
     # Opening JSON file
     f = open(path, 'r', encoding='utf-8')
@@ -77,26 +79,54 @@ def data_load2(path, argument, startdate=None, enddate=None, number_of_reviews=0
     for i in data:
         if startdate != None and enddate != None:
             review_date = get_date(i['date'])
+            graph_date = "-".join(review_date)
+        else:
+            review_date = get_date(i['date'])
+            if int(review_date[0]) >= datetime.datetime.now().year - 1:
+                graph_date = "-".join(review_date)
         if 'opinion' in argument:
             if startdate == None and enddate == None:
                 output.append(i['opinion']+i['title'])
                 score.append(i['score'])
+                if graph_date in graph_values.keys():
+                    graph_values[graph_date] += 1
+                else:
+                    graph_values[graph_date] = 1
             elif startdate[0] <= review_date[0] <= enddate[0] and startdate[1] <= review_date[1] <= enddate[1] and startdate[2] <= review_date[2] <= enddate[2]:
                 output.append(i['opinion']+i['title'])
                 score.append(i['score'])
+                if graph_date in graph_values.keys():
+                    graph_values[graph_date] += 1
+                else:
+                    graph_values[graph_date] = 1
         elif 'title' in argument:
             if startdate == None and enddate == None:
                 output.append(i['title']+i['opinion'])
                 score.append(i['score'])
+                if graph_date in graph_values.keys():
+                    graph_values[graph_date] += 1
+                else:
+                    graph_values[graph_date] = 1
             elif startdate[0] <= review_date[0] <= enddate[0] and startdate[1] <= review_date[1] <= enddate[1] and startdate[2] <= review_date[2] <= enddate[2]:
                 output.append(i['title']+i['opinion'])
                 score.append(i['score'])
+                if graph_date in graph_values.keys():
+                    graph_values[graph_date] += 1
+                else:
+                    graph_values[graph_values] = 1
         else:
             if startdate == None and enddate == None:
                 output.append(i[argument[0]])
                 score.append(i[argument[1]])
+                if graph_date in graph_values.keys():
+                    graph_values[graph_date] += 1
+                else:
+                    graph_values[graph_date] = 1
             elif startdate[0] <= review_date[0] <= enddate[0] and startdate[1] <= review_date[1] <= enddate[1] and startdate[2] <= review_date[2] <= enddate[2]:
-                print("found")
+                if graph_date in graph_values.keys():
+                    graph_values[graph_date] += 1
+                else:
+                    graph_values[graph_date] = 1                
                 output.append(i[argument][0])
                 score.append(i[argument[1]])
 
@@ -111,7 +141,7 @@ def data_load2(path, argument, startdate=None, enddate=None, number_of_reviews=0
         k += 1
     # Closing file
     f.close()
-    return output, score, k
+    return output, score, k, graph_values
 
 
 def data_load3(path, argument, output, score, start, end):
@@ -157,6 +187,7 @@ def data_load4(path, argument, startdate=None, enddate=None, number_of_reviews=0
         enddate = enddate.split('-')
     output = []
     score = []
+    graph_values = dict()
     
     # Opening JSON file
     f = open(path, 'r', encoding='utf-8')
@@ -173,34 +204,62 @@ def data_load4(path, argument, startdate=None, enddate=None, number_of_reviews=0
     for i in data:
         if startdate != None and enddate != None:
             review_date = get_date(i['date'])
+            graph_date = "-".join(review_date)
+        else:
+            review_date = get_date(i['date'])
+            if int(review_date[0]) >= datetime.datetime.now().year - 1:
+                graph_date = "-".join(review_date)
         if 'opinion' in argument:
             out = set((i['opinion']+i['title']).split(' '))
             out = {string.lower().strip('.,!;:()[]{}?/\\\'\"') for string in out}
             if startdate == None and enddate == None and (out & input1):
                 output.append(i['opinion']+i['title'])
                 score.append(i['score'])
+                if graph_date in graph_values.keys():
+                    graph_values[graph_date] += 1
+                else:
+                    graph_values[graph_date] = 1
             elif startdate != None and enddate != None and startdate[0] <= review_date[0] <= enddate[0] and startdate[1] <= review_date[1] <= enddate[1] and startdate[2] <= review_date[2] <= enddate[2] and (out & input1):
                 output.append(i['opinion']+i['title'])
                 score.append(i['score'])
+                if graph_date in graph_values.keys():
+                    graph_values[graph_date] += 1
+                else:
+                    graph_values[graph_date] = 1
         elif 'title' in argument:
             out = set((i['title']+i['opinion']).split(' '))
             out = {string.lower().strip('.,!;:()[]{}?/\\\'\"') for string in out}
             if startdate == None and enddate == None and (out & input1):
                 output.append(i['title']+i['opinion'])
                 score.append(i['score'])
+                if graph_date in graph_values.keys():
+                    graph_values[graph_date] += 1
+                else:
+                    graph_values[graph_date] = 1
             elif startdate != None and enddate != None and startdate[0] <= review_date[0] <= enddate[0] and startdate[1] <= review_date[1] <= enddate[1] and startdate[2] <= review_date[2] <= enddate[2] and (out & input1):
                 output.append(i['title']+i['opinion'])
                 score.append(i['score'])
+                if graph_date in graph_values.keys():
+                    graph_values[graph_date] += 1
+                else:
+                    graph_values[graph_date] = 1
         else:
             out = set(i[argument[0]].split(' '))
             out = {string.lower().strip('.,!;:()[]{}?/\\\'\"') for string in out}
             if startdate == None and enddate == None and (out & input1):
                 output.append(i[argument[0]])
                 score.append(i[argument[1]])
+                if graph_date in graph_values.keys():
+                    graph_values[graph_date] += 1
+                else:
+                    graph_values[graph_date] = 1
             elif startdate != None and enddate != None and startdate[0] <= review_date[0] <= enddate[0] and startdate[1] <= review_date[1] <= enddate[1] and startdate[2] <= review_date[2] <= enddate[2] and (out & input1):
-                print("found")
                 output.append(i[argument][0])
                 score.append(i[argument[1]])
+                if graph_date in graph_values.keys():
+                    graph_values[graph_date] += 1
+                else:
+                    graph_values[graph_date] = 1
 
         if k == number_of_reviews and number_of_reviews != 0:
             break
@@ -213,4 +272,4 @@ def data_load4(path, argument, startdate=None, enddate=None, number_of_reviews=0
         k += 1
     # Closing file
     f.close()
-    return output, score, k
+    return output, score, k, graph_values
