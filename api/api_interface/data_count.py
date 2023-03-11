1c5e414bacc65c092f4f20ca1854cf1d0d515704
Kevin Oyen <kevin.oyen@student.kuleuven.be>
2023-03-11 08:53:21 +0100

    data loader optimalization

--- api/api_interface/data_count.py
+++ api/api_interface/data_count.py
@@ -10,50 +10,45 @@ import numpy as np
 import time
 
 def count(input, file, startdate=None, enddate=None):
+    #count = {element: [0, 0, Sentiment_dutch()] for element in input}
     count = {element: [0, 0, Sentiment_dutch()] for element in input}
 
-    # sentences = data_load(file, 'opinion', startdate, enddate)
-    # score = data_load(file, 'score', startdate, enddate)
-    
     start_time = time.time()
-    # path, argument, output, score, start=0, end=None)
-    sentences, score, iterations = data_load4(file, ['opinion', 'score'], [], [], startdate, enddate)
+    start_time_tot = time.time()
+    if startdate != None and enddate != None:
+        sentences, score, iterations = data_load2(file, ['opinion', 'score'], startdate, enddate, 0)
+    else:
+        sentences, score, iterations = data_load4(file, ['opinion', 'score'], None, None, 0, input)
     end_time = time.time()
     print("elapsed time: "+str(end_time-start_time))
-    
-    # multiprocessing
-    
-    # chunks = [data[i:i+chunk_size] for i in range(0, len(data), chunk_size)]
-    # pool = multiprocessing.Pool(processes=5)
-    # results = pool.map(process_chunk, chunks)
-    
+    start_time = time.time()
+    total_limit = 0
     for i in range(len(sentences)):
         for word in input:
             if word in sentences[i].lower():
                 count[word][0] += 1
                 count[word][1] += int(score[i])
-                count[word][2].compute_sentiment(sentences[i])
-        print("it2: "+str(i))
-                
+                if total_limit < 100:
+                    count[word][2].compute_sentiment(sentences[i])
+                    total_limit += 1
+        #print("it2: "+str(i))
+    end_time = time.time()
+    print("elapsed time: "+str(end_time-start_time))
+    end_time_tot= time.time()
+    print("elapsed time tot: "+str(end_time_tot-start_time_tot))
+           
     for element in count.keys():
         count[element][1] = count[element][1] / count[element][0]
-    # print(count)
-    # print(count[element][2].get_sentiment(count[element][0]))
+
     # (keywords), hoe_vaak_totaal, hoe_vaak_procent, polariteit, (subjectiviteit), gem_review, tijdsvoorkomen
     keywords = input
-    # totaal = [count[element][0] for element in count.keys()]
-    # procent = [totaal[i]/iterations for i in range(len(count))]
-    # polariteit = [count[element][2].get_sentiment(count[element][0])['average_polarity'] for element in count.keys()]
-    # sentiment = [count[element][2].get_sentiment(count[element][0])['overall_sentiment'] for element in count.keys()]
-    # gem_review = [count[element][1] for element in count.keys()]
-    
     totaal = [count[element][0] for element in count]
     procent = [t / iterations for t in totaal]
     polariteit = [count[element][2].get_sentiment(t)['average_polarity'] for element, t in zip(count, totaal)]
     sentiment = [count[element][2].get_sentiment(t)['overall_sentiment'] for element, t in zip(count, totaal)]
     gem_review = [count[element][1] for element in count]
     tijdsvoorkomen = []
-    #print(keywords,totaal,procent,polariteit,sentiment,gem_review,tijdsvoorkomen)
+    print(keywords,totaal,procent,polariteit,sentiment,gem_review,tijdsvoorkomen)
     return keywords,totaal,procent,polariteit,sentiment,gem_review,tijdsvoorkomen
             
 if __name__ == "__main__":
@@ -62,7 +57,7 @@ if __name__ == "__main__":
     for i in range(len(input)):
         input[i] = input[i].lower()
 
-    file = 'data/reviews_pakket.json'
+    file = '../../data/reviews_pakket.json'
     #file = 'data/reviews_spotify.csv'
     count(input, file)
     #count(input, file, '2020-11-19', '2020-12-30')
--- api/api_interface/data_load.py
+++ api/api_interface/data_load.py
@@ -41,7 +41,6 @@ def data_load(path, argument, startdate=None, enddate=None, number_of_reviews=0)
             if startdate == None and enddate == None:
                 output.append(i[argument])
             elif startdate[0] <= review_date[0] <= enddate[0] and startdate[1] <= review_date[1] <= enddate[1] and startdate[2] <= review_date[2] <= enddate[2]:
-                print("found")
                 output.append(i[argument])
 
         if k == number_of_reviews and number_of_reviews != 0:
@@ -51,7 +50,7 @@ def data_load(path, argument, startdate=None, enddate=None, number_of_reviews=0)
         elif startdate != None and enddate != None and review_date[0] == enddate[0] and review_date[1] == enddate[1] and review_date[2] < enddate[2]:
             print(startdate, review_date, enddate)
             break
-        print("it: "+str(k))
+        #print("it: "+str(k))
         k += 1
     # Closing file
     f.close()
@@ -151,23 +150,67 @@ def data_load3(path, argument, output, score, start, end):
     f.close()
     return output, score
 
-def data_load4(path, argument, output, score, start=0, end=None):
-    with open(path, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-        
-    end = end or len(data)
+def data_load4(path, argument, startdate=None, enddate=None, number_of_reviews=0, input=[]):
+    if startdate != None:
+        startdate = startdate.split('-')
+    if enddate != None:
+        enddate = enddate.split('-')
+    output = []
+    score = []
     
-    for i, d in enumerate(data[start:end]):
+    # Opening JSON file
+    f = open(path, 'r', encoding='utf-8')
+    
+    # returns JSON object as 
+    # a dictionary
+    data = json.load(f)
+    
+    # Iterating through the json
+    # list
+    k = 1
+    
+    input1 = {string.lower() for string in input}
+    for i in data:
+        if startdate != None and enddate != None:
+            review_date = get_date(i['date'])
         if 'opinion' in argument:
-            output.append(d['opinion'] + d['title'])
-            score.append(d['score'])
+            out = set((i['opinion']+i['title']).split(' '))
+            out = {string.lower().strip('.,!;:()[]{}?/\\\'\"') for string in out}
+            if startdate == None and enddate == None and (out & input1):
+                output.append(i['opinion']+i['title'])
+                score.append(i['score'])
+            elif startdate != None and enddate != None and startdate[0] <= review_date[0] <= enddate[0] and startdate[1] <= review_date[1] <= enddate[1] and startdate[2] <= review_date[2] <= enddate[2] and (out & input1):
+                output.append(i['opinion']+i['title'])
+                score.append(i['score'])
         elif 'title' in argument:
-            output.append(d['title'] + d['opinion'])
-            score.append(d['score'])
+            out = set((i['title']+i['opinion']).split(' '))
+            out = {string.lower().strip('.,!;:()[]{}?/\\\'\"') for string in out}
+            if startdate == None and enddate == None and (out & input1):
+                output.append(i['title']+i['opinion'])
+                score.append(i['score'])
+            elif startdate != None and enddate != None and startdate[0] <= review_date[0] <= enddate[0] and startdate[1] <= review_date[1] <= enddate[1] and startdate[2] <= review_date[2] <= enddate[2] and (out & input1):
+                output.append(i['title']+i['opinion'])
+                score.append(i['score'])
         else:
-            output.append(d[argument[0]])
-            score.append(d[argument[1]])
-        
-        print("it: " + str(i))
-    
-    return output, score, i+1
+            out = set(i[argument[0]].split(' '))
+            out = {string.lower().strip('.,!;:()[]{}?/\\\'\"') for string in out}
+            if startdate == None and enddate == None and (out & input1):
+                output.append(i[argument[0]])
+                score.append(i[argument[1]])
+            elif startdate != None and enddate != None and startdate[0] <= review_date[0] <= enddate[0] and startdate[1] <= review_date[1] <= enddate[1] and startdate[2] <= review_date[2] <= enddate[2] and (out & input1):
+                print("found")
+                output.append(i[argument][0])
+                score.append(i[argument[1]])
+
+        if k == number_of_reviews and number_of_reviews != 0:
+            break
+        elif k == len(data) and number_of_reviews == 0:
+            break
+        elif startdate != None and enddate != None and review_date[0] == enddate[0] and review_date[1] == enddate[1] and review_date[2] < enddate[2]:
+            print(startdate, review_date, enddate)
+            break
+        print("it: "+str(k))
+        k += 1
+    # Closing file
+    f.close()
+    return output, score, k
